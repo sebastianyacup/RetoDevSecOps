99d98aace5e0f96e3953ffa71e763a96d120410d
seagui <seagui@bancolombia.com.co>
2024-01-15 01:18:59 -0500

    Ajustes para ms

--- app/adapters/flask_routes.py
+++ app/adapters/flask_routes.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask, jsonify, Blueprint
 from app.adapters.flask_repository import FlaskVulnerabilityRepository
 from app.core.use_cases.obtener_vulnerabilidades import ObtenerVulnerabilidades
@@ -9,13 +10,27 @@ bp = Blueprint('vulnerabilities', __name__)
 @bp.route('/vulnerabilities', methods=['GET'])
 def get_vulnerabilities():
     repository = FlaskVulnerabilityRepository()
+    vulnerabilities = []
 
-    # Obtén los reportes desde donde sea necesario (en este ejemplo, desde la función obtener_reportes_desde_pipelines)
-    reportes = obtener_reportes_desde_pipelines()
+    repo = 'sebastianyacup/RetoDevSecOps'
+    artifact_url = f'https://api.github.com/repos/{repo}/actions/artifacts'
+    headers = {'Authorization': 'Bearer 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'}
 
-    # Crea una instancia de ObtenerVulnerabilidades con el repository y ejecuta el método execute con los reportes
-    obtener_vulnerabilidades = ObtenerVulnerabilidades(repository)
-    vulnerabilities = obtener_vulnerabilidades.execute(reportes)
+    response = requests.get(artifact_url, headers=headers)
+    artifacts = response.json()['artifacts']
+
+    if artifacts:
+        artifact_id = artifacts[0]['id']
+        download_url = f'https://api.github.com/repos/{repo}/actions/artifacts/{artifact_id}/zip'
+        response = requests.get(download_url, headers=headers)
+
+        report_path = f'/path/to/report/resultados-analisis-synk.json'
+        with open(report_path, 'wb') as file:
+            file.write(response.content)
+
+        reportes = obtener_reportes_desde_pipelines(report_path)
+        obtener_vulnerabilidades = ObtenerVulnerabilidades(repository)
+        vulnerabilities.extend(obtener_vulnerabilidades.execute(reportes))
 
     return jsonify([vars(v) for v in vulnerabilities])
 
